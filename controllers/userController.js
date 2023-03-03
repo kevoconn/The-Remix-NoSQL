f5babcc69ad572f630a729012ddf1477805e13db
github_kevoconn <kcoconnor84@gmail.com>
2023-03-03 08:26:17 -0600

    seeded folder with model data

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const { User, Student } = require("../models");
+
+module.exports = {
+  // Get all courses
+  getCourses(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a course
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.courseId })
+      .select("-__v")
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a course
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a course
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.courseId })
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : Student.deleteMany({ _id: { $in: user.students } })))
+      .then(() => res.json({ message: "User deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a course
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.courseId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user with this id!" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Student = require('./Student');
+const Course = require('./User');
+
+module.exports = { Student, Course };
